17de701d4ddd4ff8a8dfd404d90020e1474ad3ea
DebelToni <anton.ivanov.hristov@gmail.com>
2025-05-13 10:57:43 +0300

    wsl config before berge

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,16 +14,17 @@ require("config.myscripts.move_upORdown_better_in_markdown")
 
 
 --Todo: make file for customisaion
+local tabs = 2
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.o.tabstop = 4
+vim.opt.tabstop = tabs
+vim.o.tabstop = tabs
 
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.tabstop = tabs -- A TAB character looks like 4 spaces
 -- vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.o.softtabstop = tabs -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = tabs -- Number of spaces inserted when indenting
 vim.o.relativenumber = true
 
 -- godot
