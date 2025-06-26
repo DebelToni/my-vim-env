a6e4bc817b020feb38e8088b1f74e3514c74b8b4
DebelToni <anton.ivanov.hristov@gmail.com>
2025-06-26 23:05:53 +0300

    sql

--- nvim/lua/plugins/SQL.lua
+++ nvim/lua/plugins/SQL.lua
@@ -3,6 +3,7 @@ return {
 	{"kristijanhusak/vim-dadbod-ui",
 		init = function()
 			vim.g.db_ui_show_help = 0
+			vim.g.db_ui_save_location = '~/Documents/rst/sql'
 		end,
 	},
 	"kristijanhusak/vim-dadbod-completion"
