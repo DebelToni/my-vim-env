82e63d736eb9f93d9d62c77795ae6e6e28f12499
DebelToni <anton.ivanov.hristov@gmail.com>
2025-10-03 22:20:51 +0300

    who knows what i changed

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -1,11 +1,11 @@
 local set_keymap = function(mode, lhs, rhs, opts)
-    local options = { noremap = true, silent = true }
-    if opts then
-      options = vim.tbl_extend('force', options, opts)
-    end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-  end
-  
+	local options = { noremap = true, silent = true }
+	if opts then
+		options = vim.tbl_extend('force', options, opts)
+	end
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
 -- set_keymap('n', 'j', 'k', { desc = "Move up" })
 -- set_keymap('v', 'j', 'k', { desc = "Move up in visual mode" })
 --
@@ -25,20 +25,20 @@ local modes = { 'n', 'i', 'v', 'x', 's', 'o' } -- Normal, Insert, Visual, Select
 -- end
 
 -- Telescope
-set_keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", {desc = "Find files"})
-set_keymap("n", "<leader>fa", "<cmd>Telescope find_files<cr>", {desc = "Find files"})
-set_keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", {desc = "Grep text in files"})
-set_keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", {desc = "List buffers"})
-set_keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", {desc = "Help tags"})
-set_keymap("n", "<leader>fc", "<cmd>Telescope commands<cr>", {desc = "List commands"})
+set_keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
+set_keymap("n", "<leader>fa", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
+set_keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Grep text in files" })
+set_keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "List buffers" })
+set_keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Help tags" })
+set_keymap("n", "<leader>fc", "<cmd>Telescope commands<cr>", { desc = "List commands" })
 
 --Tmux
-set_keymap("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", {desc = "Nav left"})
-set_keymap("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>", {desc = "Nav up"})
+set_keymap("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", { desc = "Nav left" })
+set_keymap("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>", { desc = "Nav up" })
 -- set_keymap("n", "<C-j>", "<cmd>TmuxNavigateUp<cr>", {desc = "Nav up"})
-set_keymap("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>", {desc = "Nav down"})
+set_keymap("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>", { desc = "Nav down" })
 -- set_keymap("n", "<C-k>", "<cmd>TmuxNavigateDown<cr>", {desc = "Nav down"})
-set_keymap("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", {desc = "Nav right"})
+set_keymap("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", { desc = "Nav right" })
 
 --vim.api.nvim_set_keymap('v', '<leader>s', [[:lua require('silicon').visualise_api({})<CR>]], { noremap = true, silent = true })
 --vim.keymap.set('v', '<Leader>s',  function() silicon.visualise_api() end )
@@ -55,30 +55,31 @@ vim.api.nvim_set_keymap('n', '<A-j>', ':m+1<CR>==', { noremap = true, silent = t
 vim.api.nvim_set_keymap('v', '<A-j>', ":m'>+<CR>gv=gv", { noremap = true, silent = true, desc = "Move line down" })
 vim.api.nvim_set_keymap('v', '<A-k>', ":m'<-2<CR>gv=gv", { noremap = true, silent = true, desc = "Move line up" })
 
-vim.api.nvim_set_keymap('n', '<leader>bb', ':b#<CR>', { noremap = true , desc = "Switch to previous buffer"} )
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { noremap = true, silent = true , desc = "Open diagnostics" })
+vim.api.nvim_set_keymap('n', '<leader>b', ':b#<CR>', { noremap = true, desc = "Switch to previous buffer" })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { noremap = true, silent = true, desc = "Open diagnostics" })
 
 -- set_keymap('n', 'V', '<C-v>', { desc = "Visual block mode" })
 
 vim.keymap.set('n', '<M-k>', function()
-  vim.cmd('resize +' .. (vim.v.count1 == 1 and 5 or vim.v.count1))
+	vim.cmd('resize +' .. (vim.v.count1 == 1 and 5 or vim.v.count1))
 end, { desc = 'Resize window ↑ (count-aware)', silent = true })
 
 vim.keymap.set('n', '<M-j>', function()
-  vim.cmd('resize -' .. (vim.v.count1 == 1 and 5 or vim.v.count1))
+	vim.cmd('resize -' .. (vim.v.count1 == 1 and 5 or vim.v.count1))
 end, { desc = 'Resize window ↓ (count-aware)', silent = true })
 
 vim.keymap.set('n', '<M-h>', function()
-  vim.cmd('vertical resize -' .. (vim.v.count1 == 1 and 5 or vim.v.count1))
+	vim.cmd('vertical resize -' .. (vim.v.count1 == 1 and 5 or vim.v.count1))
 end, { desc = 'Resize window ← (count-aware)', silent = true })
 
 vim.keymap.set('n', '<M-l>', function()
-  vim.cmd('vertical resize +' .. (vim.v.count1 == 1 and 5 or vim.v.count1))
+	vim.cmd('vertical resize +' .. (vim.v.count1 == 1 and 5 or vim.v.count1))
 end, { desc = 'Resize window → (count-aware)', silent = true })
 
 -- vim.api.nvim_set_keymap('n', '<leader>z', ':ZenMode<CR>', { noremap = true, silent = true, desc = "Toggle Zen Mode" })
 
-vim.api.nvim_set_keymap('n', '<leader>dm', '<cmd>NoiceDismiss<CR>', { noremap = true, silent = true, desc = "Dismiss noice messages" })
+vim.api.nvim_set_keymap('n', '<leader>dm', '<cmd>NoiceDismiss<CR>',
+	{ noremap = true, silent = true, desc = "Dismiss noice messages" })
 
 
 vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true, desc = "Exit terminal mode" })
